41a31967db20ad833f162f7b568da78ab39eeeed
Tarunutla15 <142660669+Tarunutla15@users.noreply.github.com>
2024-09-25 10:55:17 +0530

    Time: 3411 ms (20.69%), Space: 598.4 MB (8.97%) - LeetHub

---
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        prefix_count = {}  
+
+        for word in words:
+            for i in range(1, len(word) + 1):
+                prefix = word[:i]  # Extract prefix from word
+                if prefix in prefix_count:
+                    prefix_count[prefix] += 1
+                else:
+                    prefix_count[prefix] = 1
+
+        result = []
+        for word in words:
+            score = 0
+            for i in range(1, len(word) + 1):
+                prefix = word[:i]
+                score += prefix_count[prefix]  
+            result.append(score)
+
+        return result
