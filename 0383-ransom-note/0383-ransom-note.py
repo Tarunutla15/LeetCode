1f2378ce73365914e761e5ccdca8278bb1154c08
Tarunutla15 <142660669+Tarunutla15@users.noreply.github.com>
2024-11-11 11:49:21 +0530

    Time: 13 ms (81.89%), Space: 16.9 MB (5.63%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,11 @@
+from collections import Counter
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        note = [i for i in ransomNote]
-        for i in magazine:
-            if i in note:
-                note[note.index(i)]=True
-        for i in range(len(note)):
-            if note[i] is True:
-                pass
-            else:
+        ransom_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+        
+        for char, count in ransom_count.items():
+            if magazine_count[char] < count:
                 return False
         return True
