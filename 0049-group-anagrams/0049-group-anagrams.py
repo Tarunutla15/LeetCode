65d1add2731e3d229b9052d89b0e6c5253a0d6f3
Tarunutla15 <142660669+Tarunutla15@users.noreply.github.com>
2024-11-15 14:46:28 +0530

    Time: 7 ms (97.53%), Space: 19.7 MB (81.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,7 +5,4 @@ class Solution:
         for s in strs:
             key = "".join(sorted(s))
             d[key].append(s)
-        l=[]
-        for i in d.values():
-            l.append(i)
-        return l
+        return list(d.values())
