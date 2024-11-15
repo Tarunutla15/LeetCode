85cf5fd49b0880846b0ef3fb45af545ff58d3174
Tarunutla15 <142660669+Tarunutla15@users.noreply.github.com>
2024-11-15 16:08:26 +0530

    Time: 0 ms (100.00%), Space: 16.6 MB (41.12%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,7 +1,5 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s = sum(list(map(ord,s)))
-        t = sum(list(map(ord,t)))
-        return chr(t-s)
-        
-        
+        s = sum(map(ord, s))
+        t = sum(map(ord, t))
+        return chr(t - s)
